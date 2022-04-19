ef14363c61fce65959c527a804f14d3d66d094b8
RobinHaerkens <51720452+RobinHaerkens@users.noreply.github.com>
2022-04-19 09:13:17 +0200

    gegevens model files#2

--- index.php
+++ index.php
@@ -6,10 +6,12 @@ require_once("lib/artikel.php");
 /// INIT
 $db = new database();
 $art = new artikel($db->getConnection());
-
+$usr = new user($db->getConnection());
 
 /// VERWERK 
 $data = $art->selecteerArtikel(8);
+$data = $usr->selecteerUser(2);
+
 
 /// RETURN
 var_dump($data);
